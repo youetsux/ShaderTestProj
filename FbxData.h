ddb6de304eea9326abca5a3bed84ccb886d215f5
youetsux <youetsux@gmail.com>
2023-11-30 13:45:09 +0900

    ちょいすすんだ？

--- FbxData.h
+++ FbxData.h
@@ -24,14 +24,6 @@ class FbxData
 		XMFLOAT4 diffuse;
 	};
 
-	struct CONSTANT_BUFFER
-	{
-		XMMATRIX	matWVP;
-		XMMATRIX	matNormal;
-		XMFLOAT4	diffuseColor;
-		int			isTextured;
-	};
-
 	struct VERTEX
 	{
 		XMVECTOR position;//位置
@@ -43,21 +35,16 @@ class FbxData
 	int polygonCount_;	//ポリゴン数
 	int materialCount_;	//マテリアルの個数
 
-	ID3D11Buffer* pVertexBuffer_;
-	ID3D11Buffer** pIndexBuffer_;
-	ID3D11Buffer* pConstantBuffer_;
 	MATERIAL* pMaterialList_;
 	vector <int> indexCount_;
 
 	void InitVertex(fbxsdk::FbxMesh* mesh);
 	void InitIndex(fbxsdk::FbxMesh* mesh);
-	void IntConstantBuffer();
 	void InitMaterial(fbxsdk::FbxNode* pNode);
 public:
 
-	Fbx();
+	FbxData();
 	HRESULT Load(std::string fileName);
-	void    Draw(Transform& transform);
 	void    Release();
 };
 
