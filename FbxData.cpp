73d9704a394a1804d948c87e219ba9cdf20d9a62
Youetsu SATO <youetsux@gmail.com>
2023-12-05 12:53:56 +0900

    データ読み込みを書いてたところ

--- FbxData.cpp
+++ FbxData.cpp
@@ -11,8 +11,53 @@ FbxData::FbxData()
 HRESULT FbxData::Load(std::string fileName)
 {
 	
+	//マネージャを生成
+	FbxManager* pFbxManager = FbxManager::Create();
 
-    return E_NOTIMPL;
+	//インポーターを生成
+	FbxImporter* fbxImporter = FbxImporter::Create(pFbxManager, "imp");
+	fbxImporter->Initialize(fileName.c_str(), -1, pFbxManager->GetIOSettings());
+
+	//シーンオブジェクトにFBXファイルの情報を流し込む
+	FbxScene* pFbxScene = FbxScene::Create(pFbxManager, "fbxscene");
+	fbxImporter->Import(pFbxScene);
+	fbxImporter->Destroy();
+
+	//メッシュ情報を取得
+	FbxNode* rootNode = pFbxScene->GetRootNode();
+	FbxNode* pNode = rootNode->GetChild(0);
+	FbxMesh* mesh = pNode->GetMesh();
+
+	//各情報の個数を取得
+	vertexCount_ = mesh->GetControlPointsCount();	//頂点の数
+	polygonCount_ = mesh->GetPolygonCount();	//ポリゴンの数
+	materialCount_ = pNode->GetMaterialCount();
+
+	//現在のカレントディレクトリを取得
+	//char defaultCurrentDir[MAX_PATH];
+	//GetCurrentDirectory(MAX_PATH, defaultCurrentDir);
+	std::filesystem::path crrPath = std::filesystem::current_path();
+
+
+	//引数のfileNameからディレクトリ部分を取得
+	char dir[MAX_PATH];
+	_splitpath_s(fileName.c_str(), nullptr, 0, dir, MAX_PATH, nullptr, 0, nullptr, 0);
+
+	//カレントディレクトリ変更
+	SetCurrentDirectory(dir);
+
+	InitVertex(mesh);		//頂点バッファ準備
+	InitIndex(mesh);		//インデックスバッファ準備
+	IntConstantBuffer();	//コンスタントバッファ準備
+	InitMaterial(pNode);
+
+	//カレントディレクトリを元に戻す
+	SetCurrentDirectory(defaultCurrentDir);
+
+
+	//マネージャ解放
+	pFbxManager->Destroy();
+	return S_OK;
 }
 
 
