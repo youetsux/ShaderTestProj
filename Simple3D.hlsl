3e1e42712cc45a5cf4009780cd56c90f8ba6db18
y.sato <y.sato@jc-21.jp>
2023-12-18 10:10:55 +0900

    トゥーンシェーディング始めました

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -81,6 +81,15 @@ float4 PS(VS_OUT inData) : SV_Target
 	//float4 reflect = normalize(2 * NL * inData.normal - normalize(lightPosition));
 	float4 reflection = reflect(normalize(-lightPosition), inData.normal);
 	float4 specular = pow(saturate(dot(reflection, normalize(inData.eyev))), shininess) * specularColor;
+	//‚±‚Ì•Ó‚ÅŠgŽU”½ŽË‚Ì’l‚ð‚²‚É‚å‚²‚É‚å‚·‚é
+	float4 n1 = float4(1 / 4.0, 1 / 4.0, 1 / 4.0, 1);
+	float4 n2 = float4(2 / 4.0, 2 / 4.0, 2 / 4.0, 1);
+	float4 n3 = float4(3 / 4.0, 3 / 4.0, 3 / 4.0, 1);
+	float4 n4 = float4(4 / 4.0, 4 / 4.0, 4 / 4.0, 1);
+	
+	float4 tI = 0.1*step(n1, inData.color) + 0.3*step(n2, inData.color) 
+		      + 0.3*step(n3, inData.color) + 0.4*step(n4, inData.color);
+	
 	if (isTextured == 0)
 	{
 		diffuse = lightSource * diffuseColor * inData.color;
@@ -91,8 +100,8 @@ float4 PS(VS_OUT inData) : SV_Target
 		diffuse = lightSource * g_texture.Sample(g_sampler, inData.uv) * inData.color;
 		ambient = lightSource * g_texture.Sample(g_sampler, inData.uv) * ambientColor;
 	}
-	return diffuse + ambient + specular;
-
+	//return diffuse + ambient + specular;
+	return tI;
 	
 	//return g_texture.Sample(g_sampler, inData.uv);
 }
